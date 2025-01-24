7a90ebb39159c5a582477c60742fcac631d245c4
Matthew Harris <matthew.harris@unsw.edu.au>
2025-01-24 15:18:24 +1300

    update parsing for new CDS netcdf4 format

--- R/extract_era5.R
+++ R/extract_era5.R
@@ -19,19 +19,47 @@
 #'
 extract_era5 <- function(nc_filepath, coords = NULL){
   ## nc handling heavily adapted from https://renewable-analytics.netlify.app/2018/06/25/using-era-interim-in-r/#open-the-file
+  # Note: late 2024 updates to CDS and ERA5 have changed the GRIB to ncdf4 conversion, leading to altered netcdf file format.
   nc_file <- nc_open(nc_filepath)
-  # Extract coordinate information
-  lat = ncvar_get(nc_file,'latitude')
-  lon = ncvar_get(nc_file,'longitude')
-  # time handling
-  t <- ncvar_get(nc_file, "time")
-  tunits <- ncatt_get(nc_file,'time')
-  tustr <- strsplit(tunits$units, " ")
-  timestamp = as.POSIXct(t * 3600, tz = 'GMT', origin = tustr[[1]][3])
-  # Basic parsing of NC data. Frame with all attributes.
-  data<- tibble(name = attributes(nc_file$var)$names) %>%
-    bind_cols(purrr::map_df(.$name, ncatt_get, nc = nc_file)) %>% # Get basic names
-    mutate(values = purrr::map(name, ncvar_get, nc = nc_file)) # Acquire values
+  # What's the format?
+  file_format = nc_file$format
+  if(file_format == "NC_FORMAT_NETCDF4"){
+    # Post-2024 ERA5 netcdf4 file format
+    # Extract coordinate information
+    lat = ncvar_get(nc_file,'latitude')
+    lon = ncvar_get(nc_file,'longitude')
+    # time handling
+    t <- ncvar_get(nc_file, "valid_time") # time changed to 'valid_time'
+    tunits <- ncatt_get(nc_file, "valid_time")
+    tustr <- strsplit(tunits$units, " ")#[[1]][3]# %>% lapply(.,'[',3)
+    # init_date = as.Date(tustr[[1]][3])
+    time_steps = tustr[[1]][1]
+    if(time_steps == 'hours'){
+      timestamp = as.POSIXct(t * 3600, tz = "GMT", origin = tustr[[1]][3]) # tustr[[1]][3] is the init date
+    } else if(time_steps == 'seconds'){
+      timestamp = as.POSIXct(t, tz = "GMT", origin = tustr[[1]][3])
+    }
+    # VAR now has three elements not one.
+    # This code now assumes you're after not the number and expver vars.
+    data <- tibble(name = attributes(nc_file$var)$names[which(attributes(nc_file$var)$names != "number" & attributes(nc_file$var)$names != "expver")]) %>%
+      bind_cols(purrr::map_df(.$name, ncatt_get, nc = nc_file)) %>%
+      mutate(values = purrr::map(name, ncvar_get, nc = nc_file))
+  } else {
+    # Pre-late 2024 ERA5 netcdf4 file format
+    # Extract coordinate information
+    lat = ncvar_get(nc_file,'latitude')
+    lon = ncvar_get(nc_file,'longitude')
+    # time handling
+    t <- ncvar_get(nc_file, "time")
+    tunits <- ncatt_get(nc_file,'time')
+    tustr <- strsplit(tunits$units, " ")
+    timestamp = as.POSIXct(t * 3600, tz = 'GMT', origin = tustr[[1]][3])
+    # Basic parsing of NC data. Frame with all attributes.
+    data<- tibble(name = attributes(nc_file$var)$names) %>%
+      bind_cols(purrr::map_df(.$name, ncatt_get, nc = nc_file)) %>% # Get basic names
+      mutate(values = purrr::map(name, ncvar_get, nc = nc_file)) # Acquire values
+
+  }
   nc_close(nc_file)
   # pre-allocate DF for desired grid square
   df_int <-expand.grid(lon = lon, lat = lat, timestamp = timestamp, name = data$name) %>%
